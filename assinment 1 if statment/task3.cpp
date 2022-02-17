7a8a2972248f8a7a0c9e99d24c1c836162e44f21
mohamedmohdy <66391236+mohamedmohdy@users.noreply.github.com>
2022-02-17 22:35:45 +0200

    Update task3.cpp

--- assinment 1 if statment/task3.cpp
+++ assinment 1 if statment/task3.cpp
@@ -1,3 +1,17 @@
+/*
+write a program that ask user to insert 3 integers then print the average as
+decimal number
+● input
+Please enter first number:
+6
+Please enter second number
+4
+Please enter third number
+4
+● output
+The average = 4.66667
+*/
+
 #include <iostream>
 using namespace std;
 int main()
