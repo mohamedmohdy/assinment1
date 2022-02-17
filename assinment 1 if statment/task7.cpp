bd781ef446dab839cc401be9307e9c4d96a76223
mohamedmohdy <66391236+mohamedmohdy@users.noreply.github.com>
2022-02-17 22:41:21 +0200

    Update task7.cpp

--- assinment 1 if statment/task7.cpp
+++ assinment 1 if statment/task7.cpp
@@ -1,3 +1,16 @@
+/*
+Write a program that allows the user to insert an integer then print negative if it
+is a negative number otherwise print positive.
+● Example
+Input -5
+Output negative
+● Example2
+Input 10
+Output positive
+*/
+
+
+
 #include <iostream>
 using namespace std;
 int main()
