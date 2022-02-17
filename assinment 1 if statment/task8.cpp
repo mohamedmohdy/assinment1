9171a39eb99648704fba48630fd446065b8192f5
mohamedmohdy <66391236+mohamedmohdy@users.noreply.github.com>
2022-02-17 22:42:02 +0200

    Update task8.cpp

--- assinment 1 if statment/task8.cpp
+++ assinment 1 if statment/task8.cpp
@@ -1,3 +1,18 @@
+/*
+Write a program that reads 5 numbers and sum of all odd values between
+them.
+● Test Data :
+Input the first number: 11
+Input the second number: 17
+Input the third number: 13
+Input the fourth number: 12
+Input the fifth number: 5
+● Expected Output:
+Sum of all odd values: 46
+*/
+
+
+
 #include <iostream>
 using namespace std;
 int main()
