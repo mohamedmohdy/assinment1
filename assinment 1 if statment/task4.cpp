4e178b3d7eb680d836e969405f4c2808b58e0dd9
mohamedmohdy <66391236+mohamedmohdy@users.noreply.github.com>
2022-02-17 22:36:52 +0200

    Update task4.cpp

--- assinment 1 if statment/task4.cpp
+++ assinment 1 if statment/task4.cpp
@@ -1,3 +1,15 @@
+/*
+Write a program to compute the perimeter and area of a rectangle. Your code
+should ask user to insert the width and the height
+● input(example)
+Width = 5
+Height = 3
+● Output
+The perimeter = 16
+The area = 15
+*/
+
+
 #include <iostream>
 using namespace std;
 int main()
