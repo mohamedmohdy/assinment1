cc8e851284a9cf6675ec9200877979852eaa2f31
mohamedmohdy <66391236+mohamedmohdy@users.noreply.github.com>
2022-02-17 22:44:19 +0200

    Update task11.cpp

--- assinment 1 if statment/task11.cpp
+++ assinment 1 if statment/task11.cpp
@@ -1,3 +1,15 @@
+/*
+11.Search for ternary operator and use it to write a program that allows the
+user to insert integer number then Even or Odd
+● Example
+Input 8
+Output even
+● Example 2
+Input 7
+Output odd
+*/
+
+
 #include <iostream>
 using namespace std;
 int main()
