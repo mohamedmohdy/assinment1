2d7ba52fdf8dae46576389968134cc7789bedc5e
mohamedmohdy <66391236+mohamedmohdy@users.noreply.github.com>
2022-02-17 22:33:14 +0200

    Update task2.cpp

--- assinment 1 if statment/task2.cpp
+++ assinment 1 if statment/task2.cpp
@@ -1,3 +1,17 @@
+
+/*
+write a program that ask user to insert 3 numbers then print the sum
+● input
+Please enter first number:
+5
+Please enter second number
+3
+Please enter third number
+4
+● output
+Summation of 3 integers is 12
+*/
+
 #include <iostream>
 using namespace std;
 int main()
