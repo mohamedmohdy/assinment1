8f7146920a37a72e3275ea8f7fe3bad9208623be
mohamedmohdy <66391236+mohamedmohdy@users.noreply.github.com>
2022-02-17 22:40:30 +0200

    Update task6.cpp

--- assinment 1 if statment/task6.cpp
+++ assinment 1 if statment/task6.cpp
@@ -1,3 +1,15 @@
+
+/*
+Write a program that take number from user then print yes if that number can
+divide by 3 and 4 otherwise print no
+● Example1
+Input 12
+Output Yes
+● Example 2
+Input 9
+Output No
+*/
+
 #include <iostream>
 using namespace std;
 int main()
