604e4240750c8bbb16caccd09c2465769106a697
mohamedmohdy <66391236+mohamedmohdy@users.noreply.github.com>
2022-02-17 22:45:49 +0200

    Update task13.cpp

--- assinment 1 if statment/task13.cpp
+++ assinment 1 if statment/task13.cpp
@@ -1,3 +1,18 @@
+/*
+13.Write a program that take 3 integer from user then print the max element and
+the min element.
+● Example
+Input 7 8 5
+Output
+max element = 8
+min element = 5
+● Example2
+Input 3 6 9
+Output
+Max element = 9
+Min element = 3
+*/
+
 #include <iostream>
 using namespace std;
 int main()
